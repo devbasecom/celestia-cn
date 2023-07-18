f176dcd94aea1df3c35216197e668dfced3bbed5
IanXu <jueduizone@gmail.com>
2023-07-19 00:53:33 +0800

    add related js

---
+++ src/versions/mocha_versions.js
@@ -0,0 +1,9 @@
+const mochaVersions = Object.freeze({
+    "app-latest-tag": "v1.0.0-rc9",
+    "app-latest-sha": "ebfc4168cb5b5ac17a92ea0dbb9e50c78d8ae1bc",
+    "core-latest-tag": "v1.24.0-tm-v0.34.28",
+    "core-latest-sha": "d24c81afe0ecc4ccfbb7eb543a45a11794f04db4",
+    "node-latest-tag": "v0.11.0-rc8",
+    "node-latest-sha": "e41f62662cf1844ba52d7a93d03cf79b6e363897",
+});
+export default mochaVersions;
